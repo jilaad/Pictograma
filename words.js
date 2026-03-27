1f5cb919294d69fb103fb79e7486cb647face5a5
Victor Wilcox <vnwilcox4@gmail.com>
2026-03-26 23:00:07 -0400

    Added several words

--- words.js
+++ words.js
@@ -33,5 +33,71 @@ const words = [
     { word: "agua", images: [
         { file: "images/Glass-of-water.jpg", accepts: ["agua","vaso"]},
         { file: "images/water.jpg"}
+    ]},
+    { word: "tarjeta de crédito", images: [
+        { file: "images/credit_card.jpg" }
+    ]},
+    { word: "camarero", images: [
+        { file: "images/waiter.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter02.jpg", accepts: ["camarero","mesero"]}
+    ]},
+    { word: "mesero", images: [
+        { file: "images/waiter.jpg", accepts: ["camarero","mesero"]},
+        { file: "images/waiter02.jpg", accepts: ["camarero","mesero"]}
+    ]},
+    { word: "mesa", images: [
+        { file: "images/table01.jpg" },
+        { file: "images/table02.webp" },
+        { file: "images/table03.jpg" },
+        { file: "images/table04.jpg" }
+    ]},
+    { word: "cabina", images: [
+        { file: "images/booth01.webp" },
+        { file: "images/booth02.png" },
+        { file: "images/booth03.jpg" },
+        { file: "images/booth04.webp" }
+    ]},
+    { word: "servilleta", images: [
+        { file: "images/napkin01.jpg" },
+        { file: "images/napkin02.webp" },
+        { file: "images/napkin03.webp" },
+        { file: "images/napkin04.webp" }
+    ]},
+    { word: "cuchara", images: [
+        { file: "images/spoon01.jpg" },
+        { file: "images/spoon02.webp" },
+        { file: "images/spoon03.jpg" },
+        { file: "images/spoon04.webp" }
+    ]},
+    { word: "cuchillo", images: [
+        { file: "images/knife01.jpg" },
+        { file: "images/knife02.webp" },
+        { file: "images/knife03.webp" },
+        { file: "images/knife04.webp" }
+    ]},
+    { word: "menú", images: [
+        { file: "images/menu01.webp" },
+        { file: "images/menu02.webp" },
+        { file: "images/menu03.webp" },
+        { file: "images/menu04.jpg" }
+    ]},
+    { word: "cuenta", images: [
+        { file: "images/check_bill01.jpg" },
+        { file: "images/check_bill03.jpg" },
+        { file: "images/check_bill04.jpg" }
+    ]},
+    { word: "plato", images: [
+        { file: "images/plate01.webp" },
+        { file: "images/plate02.jpg" },
+        { file: "images/plate03.webp" },
+        { file: "images/plate04.jpg" }
+    ]},
+    { word: "tazón", images: [
+        { file: "images/bowl01.webp" },
+        { file: "images/bowl02.webp" }
+    ]},
+    { word: "bufé", images: [
+        { file: "images/buffet01.jpg" },
+        { file: "images/buffet02.webp" }
     ]}
 ];
